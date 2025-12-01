7fea3951e69baf87cb3473eb2dd09cde0ce987b4
santostzk <2006rafaelsantos@gmail.com>
2025-11-30 21:37:20 -0300

    Inicio do mapeamento e aplicação do javascript

---
+++ logic/mapping-initial.js
@@ -0,0 +1,21 @@
+//selecting files usind dom and staring into array
+
+const arrayOfFiles = document.querySelectorAll(".files");
+
+const filesNameArray = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+let fileNumber = 0;
+
+for(const i of arrayOfFiles){
+  
+  let counter = 1;
+  for (const el of i.children) {
+     el.setAttribute("id", filesNameArray[fileNumber] + counter);
+     counter++;
+  }
+  fileNumber++;
+}
+
+for(const i of arrayOfFiles){
+    console.log(i.children);
+}
